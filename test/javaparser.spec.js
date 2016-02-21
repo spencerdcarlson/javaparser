ca2bbd01d43abc8d5198d051cbab440df7e0bd47
Spencer Carlson <spencerdcarlson@gmail.com>
2016-02-20 21:37:48 -1000

    update tests

--- test/javaparser.spec.js
+++ test/javaparser.spec.js
@@ -2,13 +2,12 @@
 require('jasmine-expect');
 
 if( typeof JavaParser === 'undefined' ) {
-  var JavaParser = require('../javaparser');
+  var JavaParser = require('../src/node/javaparser');
 }
 
 describe('JavaParser', function(){
 
   it('something must be done', function(){
-    // expect(JavaParser).toHaveMethod('parse');
     expect(JavaParser).toBeDefined();
     expect(JavaParser).toBe(JavaParser);
   });
@@ -23,5 +22,15 @@ describe('JavaParser', function(){
 		done();
 	});
   });
+
+ //  it('parse a java stream', (done) => {
+ //  	const stream = "// $ javac Announce.java && java Announce\n class Announce {} \/**\n* 1.7\n* Chapter 1\n* Question 7\n* What is the result? (Choose all that apply.)\n* A. Compilation succeeds\n* B. Compilation fails with an error on line 6\n* C. Compilation fails with an error on line 7\n**\/\n\n\/**\n* Answer: C D\n* Variable names cannot begin with a #, and an \n* array declaration can't include a size without \n* an instantiation. The rest of the code is valid.\n**\/\n\n";
+ //  	JavaParser.parse(stream, (err, data) => {
+	// 	expect(data.inline).toBeNonEmptyArray();
+	// 	expect(data.multiline).toBeNonEmptyArray();
+	// 	expect(data.code).toBeTruthy();
+	// 	done();
+	// });
+ //  });
 
 });
